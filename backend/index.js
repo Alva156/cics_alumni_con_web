f07e07fbf220f4b85a398b87fd85cebeb968be42
Cid Alvarico <andreicimoune.alvarico.cics@ust.edu.ph>
2024-09-07 18:03:39 +0800

    db connected

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 6001;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cicsalumnicon.avfcd.mongodb.net/?retryWrites=true&w=majority&appName=cicsalumnicon`
+  )
+  .then(() => console.log("MongoDB Connected Successfully"))
+  .catch((error) => console.log("Error connecting to MongoDB", error));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});

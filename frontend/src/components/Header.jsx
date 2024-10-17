25b3ffe00ff79e211af00a2ef597becbdfa04b55
Cid Alvarico <andreicimoune.alvarico.cics@ust.edu.ph>
2024-10-17 20:39:37 +0800

    navbar and alumni

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,15 @@
 import React from "react";
+import alumniconnectlogo2 from "../assets/alumniconnectlogo2.png";
 import cicslogo from "../assets/cicslogo.png";
-import alumniconnectlogo from "../assets/alumniconnectlogo.png";
 
 const Header = () => {
   return (
     <header className="bg-hgray w-full mx-auto md:px-12 py-3 flex items-center justify-between px-2">
       {/* Left Side */}
       <div className="flex items-center">
-        <img
-          src={cicslogo}
-          alt="CICS Logo"
-          className="h-11  mr-6 ml-4 md:h-16"
-        />
-        <div className="col">
-          <p className="text-black font-light italic text-end text-xxs mt-1 md:text-xs  ">
+        <img src={cicslogo} alt="UST Logo" className="h-11 mr-4 ml-4 md:h-16" />
+        <div className="col mr-10">
+          <p className="text-black font-light italic text-xs md:text-sm mt-2">
             University of Santo Tomas
           </p>
           <p className="text-black font-bold italic text-xs md:text-sm mb-1">
@@ -21,11 +17,19 @@ const Header = () => {
           </p>
         </div>
       </div>
-      <img
-        src={alumniconnectlogo}
-        alt="Alumni Connect Logo"
-        className="h-11 ml-5 mr-4 md:h-16"
-      />
+
+      {/* Right Side */}
+      <div className="flex items-center">
+        <p className="font-bold text-lg md:text-3xl">
+          <span className="text-[#2d2b2b]">Alumni</span>
+          <span className="text-[#be142e]"> Connect</span>
+        </p>
+        <img
+          src={alumniconnectlogo2}
+          alt="CICS Logo"
+          className="h-11 ml-2 mr-2 md:h-16"
+        />
+      </div>
     </header>
   );
 };

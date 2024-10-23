195a703787459afb426aa707782a4dfa1cc035fe
Cid Alvarico <andreicimoune.alvarico.cics@ust.edu.ph>
2024-10-23 13:02:14 +0800

    filter issues on batch and fields

--- frontend/src/pages/admin/AdminReports.jsx
+++ frontend/src/pages/admin/AdminReports.jsx
@@ -10,6 +10,9 @@ function AdminReports() {
   const backendUrl = import.meta.env.VITE_BACKEND_URL;
   const [selectedPrograms, setSelectedPrograms] = useState(["All Programs"]);
   const [selectedFields, setSelectedFields] = useState(["All Fields"]);
+  const [selectedColleges, setSelectedColleges] = useState(["All Colleges"]);
+  const [selectedBatches, setSelectedBatches] = useState(["All Batches"]);
+  const [availableBatches, setAvailableBatches] = useState([]);
   const [openDropdown, setOpenDropdown] = useState("");
   const [alumni, setAlumni] = useState([]);
 
@@ -34,6 +37,7 @@ function AdminReports() {
 
   const fieldToKeyMap = {
     Profession: "profession",
+    College: "college",
     "College Program": "collegeProgram",
     Specialization: "specialization",
     "Year Started on College Program": "yearStartedCollege",
@@ -55,12 +59,141 @@ function AdminReports() {
 
   const availableFields = Object.keys(fieldToKeyMap);
   const availablePrograms = [
+    "Accountancy",
+    "Accounting Information System",
+    "Management Accounting",
+    "Architecture",
+    "Asian Studies",
+    "Behavioral Science",
+    "Communication",
+    "Creative Writing",
+    "Economics",
+    "English Language Studies",
+    "History",
+    "Journalism",
+    "Legal Management",
+    "Literature",
+    "Philosophy",
+    "Political Science",
+    "Sociology",
+    "Business Administration, major in Business Economics",
+    "Business Administration, major in Financial Management",
+    "Business Administration, major in Human Resource Management",
+    "Business Administration, major in Marketing Management",
+    "Entrepreneurship",
+    "Secondary Education Major in English",
+    "Secondary Education Major in Filipino",
+    "Secondary Education Major in Mathematics",
+    "Secondary Education Major in Religious and Values Education",
+    "Secondary Education Major in Science",
+    "Secondary Education Major in Social Studies",
+    "Early Childhood Education",
+    "Elementary Education",
+    "Special Needs Education, major in Early Childhood Education",
+    "Food Technology",
+    "Nutrition and Dietetics",
+    "Bachelor of Library and Information Science",
+    "Chemical Engineering",
+    "Civil Engineering",
+    "Electrical Engineering",
+    "Electronics Engineering",
+    "Industrial Engineering",
+    "Mechanical Engineering",
+    "Fine Arts, major in Advertising Arts",
+    "Fine Arts, major in Industrial Design",
+    "Interior Design",
+    "Fine Arts, major in Painting",
     "Computer Science",
     "Information Systems",
     "Information Technology",
+    "Basic Human Studies",
+    "Doctor of Medicine",
+    "Master in Clinical Audiology",
+    "Master in Pain Management",
+    "Performance, major in Bassoon",
+    "Performance, major in Choral Conducting",
+    "Performance, major in Clarinet",
+    "Composition",
+    "Performance, major in Double Bass",
+    "Performance, major in Flute",
+    "Performance, major in French Horn",
+    "Performance, major in Guitar",
+    "Jazz",
+    "Musicology",
+    "Music Education",
+    "Music Theatre",
+    "Music Technology",
+    "Performance, major in Oboe",
+    "Performance, major in Orchestral Conducting",
+    "Performance, major in Percussion",
+    "Performance, major in Piano",
+    "Performance, major in Saxophone",
+    "Performance, major in Trombone",
+    "Performance, major in Trumpet",
+    "Performance, major in Tuba",
+    "Performance, major in Viola",
+    "Performance, major in Violin",
+    "Performance, major in Violoncello",
+    "Performance, major in Voice",
+    "Nursing",
+    "Biochemistry",
+    "Medical Technology",
+    "Pharmacy",
+    "Pharmacy, major in Clinical Pharmacy",
+    "Doctor of Pharmacy",
+    "Fitness and Sports Management",
+    "Occupational Therapy",
+    "Physical Therapy",
+    "Speech-Language Pathology",
+    "Sports Science",
+    "Applied Mathematics, major in Actuarial Science",
+    "Applied Physics, major in Instrumentation",
+    "Biology, major in Environmental Biology",
+    "Biology, major in Medical Biology",
+    "Bachelor of Science major in Molecular Biology and Biotechnology",
+    "Chemistry",
+    "Data Science and Analytics",
+    "Microbiology",
+    "Psychology",
+    "Hospitality Management, major in Culinary Entrepreneurship",
+    "Hospitality Management, major in Hospitality Leadership",
+    "Tourism Management, major in Recreation and Leisure Management",
+    "Tourism Management, major in Travel Operation and Service Management",
+    "Doctor of Canon Law",
+    "Licentiate in Canon Law",
+    "Bachelor of Canon Law",
+    "Doctor of Philosophy",
+    "Licentiate in Philosophy",
+    "Bachelor of Philosophy (Classical)",
+    "Doctor of Sacred Theology",
+    "Licentiate in Sacred Theology",
+    "Bachelor of Sacred Theology",
+  ];
+  const availableColleges = [
+    "UST-AMV College of Accountancy",
+    "College of Architecture",
+    "Faculty of Arts and Letters",
+    "Faculty of Civil Law",
+    "College of Commerce and Business Administration",
+    "College of Education",
+    "Faculty of Engineering",
+    "College of Fine Arts and Design",
+    "College of Information and Computing Sciences",
+    "Faculty of Medicine and Surgery",
+    "Conservatory of Music",
+    "College of Nursing",
+    "Faculty of Pharmacy",
+    "Institute of Physical Education and Athletics",
+    "College of Rehabilitation Sciences",
+    "College of Science",
+    "College of Tourism and Hospitality Management",
+    "Faculty of Canon Law",
+    "Faculty of Philosophy",
+    "Faculty of Sacred Theology",
   ];
 
   // Fetch Alumni Data
+
   useEffect(() => {
     const fetchAlumni = async () => {
       try {
@@ -69,13 +202,21 @@ function AdminReports() {
         });
 
         const alumniData = response.data.alumni || response.data;
+
         alumniData.forEach((alum) => {
           alum.yearStartedCollege = formatDate(alum.yearStartedCollege);
           alum.yearGraduatedCollege = formatDate(alum.yearGraduatedCollege);
-          // Format other date fields as needed
         });
 
         setAlumni(alumniData);
+
+        // Extract unique graduation years for the batch dropdown
+        const uniqueBatches = [
+          ...new Set(
+            alumniData.map((alum) => alum.yearGraduatedCollege.split("-")[0]) // Extract only the year
+          ),
+        ];
+        setAvailableBatches(uniqueBatches);
       } catch (error) {
         console.error("Error fetching alumni:", error);
       }
@@ -101,6 +242,42 @@ function AdminReports() {
 
   const toggleDropdown = (type) => {
     setOpenDropdown((prev) => (prev === type ? "" : type));
+  };
+  const handleCollegeSelection = (event) => {
+    const selectedCollege = event.target.value;
+    setSelectedColleges((prevSelectedColleges) => {
+      if (selectedCollege === "All Colleges") {
+        return ["All Colleges"];
+      }
+      if (prevSelectedColleges.includes("All Colleges")) {
+        return [selectedCollege];
+      }
+      if (prevSelectedColleges.includes(selectedCollege)) {
+        return prevSelectedColleges.filter(
+          (college) => college !== selectedCollege
+        );
+      } else {
+        return [...prevSelectedColleges, selectedCollege];
+      }
+    });
+  };
+
+  // Handle Batch Selection
+  const handleBatchSelection = (event) => {
+    const selectedBatch = event.target.value;
+    setSelectedBatches((prevSelectedBatches) => {
+      if (selectedBatch === "All Batches") {
+        return ["All Batches"];
+      }
+      if (prevSelectedBatches.includes("All Batches")) {
+        return [selectedBatch];
+      }
+      if (prevSelectedBatches.includes(selectedBatch)) {
+        return prevSelectedBatches.filter((batch) => batch !== selectedBatch);
+      } else {
+        return [...prevSelectedBatches, selectedBatch];
+      }
+    });
   };
 
   const handleProgramSelection = (event) => {
@@ -144,12 +321,20 @@ function AdminReports() {
     return path.split(".").reduce((acc, part) => acc && acc[part], obj);
   };
 
-  // Filtered data based on selected programs
-  const filteredData = alumni.filter(
-    (row) =>
+  // Filtered data based on selected programs, colleges, and batches
+  const filteredData = alumni.filter((row) => {
+    const programMatches =
       selectedPrograms.includes("All Programs") ||
-      selectedPrograms.includes(row.collegeProgram)
-  );
+      selectedPrograms.includes(row.collegeProgram);
+    const collegeMatches =
+      selectedColleges.includes("All Colleges") ||
+      selectedColleges.includes(row.college);
+    const batchMatches =
+      selectedBatches.includes("All Batches") ||
+      selectedBatches.includes(row.yearGraduatedCollege.split("-")[0]); // Match by year only
+
+    return programMatches && collegeMatches && batchMatches;
+  });
 
   const generatePDF = async () => {
     const doc = new jsPDF({
@@ -257,12 +442,111 @@ function AdminReports() {
 
       <div className="text-sm mb-4">Filters:</div>
       <div className="sm:flex block sm:space-x-4">
+        {/* College Dropdown */}
+        <div className="relative mb-6">
+          <button
+            onClick={() => toggleDropdown("colleges")}
+            className="border border-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-light rounded-lg text-sm px-5 py-2.5 flex justify-between items-center sm:w-64 w-full relative bg-transparent"
+          >
+            <span>College</span>
+            <svg
+              className={`w-2.5 h-2.5 ms-3 absolute right-4 transition-transform duration-300 ease-in-out ${
+                openDropdown === "colleges" ? "rotate-180" : ""
+              }`}
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 10 6"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 1 4 4 4-4"
+              />
+            </svg>
+          </button>
+          {openDropdown === "colleges" && (
+            <div
+              ref={dropdownRef}
+              className="z-10 absolute sm:w-64 w-full bg-white divide-y divide-gray-100 rounded-lg shadow mt-2"
+            >
+              <ul className="p-3 space-y-3 text-sm text-gray-700">
+                {["All Colleges", ...availableColleges].map((college, idx) => (
+                  <li key={idx} className="flex items-center">
+                    <input
+                      type="checkbox"
+                      value={college}
+                      checked={selectedColleges.includes(college)}
+                      onChange={handleCollegeSelection}
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
+                    />
+                    <label className="ms-2 sm:text-xs font-medium">
+                      {college}
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
+
+        {/* Batch Dropdown */}
+        <div className="relative mb-6">
+          <button
+            onClick={() => toggleDropdown("batches")}
+            className="border border-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-light rounded-lg text-sm px-5 py-2.5 flex justify-between items-center sm:w-64 w-full relative bg-transparent"
+          >
+            <span>Batch (Year Graduated)</span>
+            <svg
+              className={`w-2.5 h-2.5 ms-3 absolute right-4 transition-transform duration-300 ease-in-out ${
+                openDropdown === "batches" ? "rotate-180" : ""
+              }`}
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 10 6"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 1 4 4 4-4"
+              />
+            </svg>
+          </button>
+          {openDropdown === "batches" && (
+            <div
+              ref={dropdownRef}
+              className="z-10 absolute sm:w-64 w-full bg-white divide-y divide-gray-100 rounded-lg shadow mt-2"
+            >
+              <ul className="p-3 space-y-3 text-sm text-gray-700">
+                {["All Batches", ...availableBatches].map((batch, idx) => (
+                  <li key={idx} className="flex items-center">
+                    <input
+                      type="checkbox"
+                      value={batch}
+                      checked={selectedBatches.includes(batch)}
+                      onChange={handleBatchSelection}
+                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
+                    />
+                    <label className="ms-2 sm:text-xs font-medium">
+                      {batch} {/* Display only the year */}
+                    </label>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
         <div className="relative mb-6">
           <button
             onClick={() => toggleDropdown("programs")}
             className="border border-black focus:ring-4 focus:outline-none focus:ring-blue-300 font-light rounded-lg text-sm px-5 py-2.5 flex justify-between items-center sm:w-64 w-full relative bg-transparent"
           >
-            <span>Program</span>
+            <span>College Program</span>
             <svg
               className={`w-2.5 h-2.5 ms-3 absolute right-4 transition-transform duration-300 ease-in-out ${
                 openDropdown === "programs" ? "rotate-180" : ""

0d381aca7c36801c840de95dccf1142fbc1c5c1d
Cid Alvarico <andreicimoune.alvarico.cics@ust.edu.ph>
2024-10-28 16:24:13 +0800

    deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,47 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "CICS Alumni Connect",
+        short_name: "Alumni",
+        description: "Connect with the UST Alumni",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "/pwa-maskable-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/pwa-maskable-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+      devOptions: {
+        enabled: true, // Allows testing of PWA features in development mode.
+      },
+    }),
+  ],
   build: {
     outDir: "dist",
   },

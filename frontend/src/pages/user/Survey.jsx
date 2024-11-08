e37642795221c952a24ba610ee4146a8e1279d3c
clairecruuz <alessandracruzclaire@gmail.com>
2024-11-08 18:28:56 +0800

    survey buttons

--- frontend/src/pages/user/Survey.jsx
+++ frontend/src/pages/user/Survey.jsx
@@ -390,24 +390,21 @@ function Survey() {
             ))}
 
             {/* BOTTOM BUTTONS */}
-            <div className="flex justify-center mt-16 space-x-3">
-              <div>
-                <button
-                  className="btn md:w-64 w-52 bg-fgray text-white"
-                  onClick={closeModal}
-                >
-                  Cancel
-                </button>
-              </div>
-              <div>
-                <button
-                  className="btn md:w-64 w-52 bg-green text-white"
-                  onClick={handleSaveResponse}
-                >
-                  Save
-                </button>
-              </div>
-            </div>
+<div className="flex justify-center gap-2 mt-8">
+  <button
+    className="btn btn-sm w-28 md:btn-md md:w-52 lg:w-60 bg-[#3D3C3C] text-white px-4 py-2 md:px-6 md:py-3"
+    onClick={closeModal}
+  >
+    Cancel
+  </button>
+  <button
+    className="btn btn-sm w-28 md:btn-md md:w-52 lg:w-60 bg-green text-white px-4 py-2 md:px-6 md:py-3"
+    onClick={handleSaveResponse}
+  >
+    Save
+  </button>
+</div>
+
           </div>
         </div>
       )}

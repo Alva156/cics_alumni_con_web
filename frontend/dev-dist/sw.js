0b8cba73a5c1b51e0b2b203c83979db06d87dc02
Cid Alvarico <andreicimoune.alvarico.cics@ust.edu.ph>
2024-11-21 21:50:22 +0800

    remove json files

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.j2gk8gi7c3"
+    "revision": "0.tttng058u78"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
